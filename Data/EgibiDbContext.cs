2e9dde06b8ed9c7b398d9c98b86c019c16e7ed31
ahubbard <adam@meta.clinic>
2024-11-28 05:59:18 -0600

    some db setup

---
+++ Data/EgibiDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+
+namespace egibi_api.Data
+{
+    public class EgibiDbContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public EgibiDbContext(DbContextOptions<EgibiDbContext> options) : base(options) { }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+#nullable disable
+using egibi_api.Data;
+using egibi_api.Services;
+using Microsoft.EntityFrameworkCore;
 
 namespace egibi_api
 {
@@ -5,11 +9,46 @@ namespace egibi_api
     {
         public static void Main(string[] args)
         {
+            var CorsPolicyDev = "_corsPolicyDev";
             var builder = WebApplication.CreateBuilder(args);
+            string env = builder.Environment.EnvironmentName;
+            string dbConnectionString = "";
+
+            if (builder.Environment.IsProduction())
+            {
+                dbConnectionString = builder.Configuration.GetConnectionString("prod_connectionstring");
+
+                builder.Services.AddDbContextPool<EgibiDbContext>(options =>
+                    options.UseNpgsql(builder.Configuration.GetConnectionString(dbConnectionString)));
+            }
+
+            if (builder.Environment.IsDevelopment())
+            {
+                dbConnectionString = builder.Configuration.GetConnectionString("EgibiDb");
+
+                builder.Services.AddDbContextPool<EgibiDbContext>(options =>
+                    options.UseNpgsql(builder.Configuration.GetConnectionString(dbConnectionString)));
+
+                builder.Services.AddCors(options =>
+                {
+                    options.AddPolicy(name: CorsPolicyDev,
+                        policy =>
+                        {
+                            policy.AllowAnyOrigin();
+                            policy.AllowAnyHeader();
+                            policy.AllowAnyMethod();
+                        });
+                });
+            }
+
 
             // Add services to the container.
+            builder.Services.AddScoped<OverviewService>();
+
 
             builder.Services.AddControllers();
+
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -23,12 +62,15 @@ namespace egibi_api
                 app.UseSwaggerUI();
             }
 
+            if (builder.Environment.IsDevelopment())
+            {
+                app.UseCors(CorsPolicyDev);
+            }
+
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
-
             app.MapControllers();
+
 
             app.Run();
         }
